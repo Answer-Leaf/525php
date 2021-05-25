31059370873278396a48a6858d395afd7cbfc94d
Answer <Answer@after.com>
2021-05-25 15:36:41 +0800

    初始化

---
+++ demo1.php
@@ -0,0 +1,32 @@
+<?php
+
+	
+	
+	
+	
+	
+	
+	
+	/*$a='';
+	if($a){
+	echo '真';	
+	}else{
+		echo '假的';	
+	}*/
+    
+	/*echo 'hello world';
+	echo "\n";
+	echo time();
+	echo "\n";
+	function sum($a,$b){
+		return $a+$b;
+	}
+	$num1=11;
+	$num2=22;
+	$num3=sum($num1,$num2);
+	echo "$num3=$num1+$num2";
+	echo __FILE__;
+	echo "\n";
+	echo __LINE__;*/
+	
+	
